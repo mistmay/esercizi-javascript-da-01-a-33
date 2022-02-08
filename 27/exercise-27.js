3a9de4a9b5091dc3da4a2314dc5d3d1fd4996957
Giuseppe Marchesiello <94814616+mistmay@users.noreply.github.com>
2022-02-08 19:01:40 +0100

    Update exercise-27.js

--- 27/exercise-27.js
+++ 27/exercise-27.js
@@ -2,14 +2,6 @@ const person = {
   firstName: 'John',
   lastName: 'Doe'
 };
-/*
-I have to put the solution in a comment
-because Beautify is not recognizing the syntax
-for the optional-chain and it does not let me save
-without changing the formatation of the code
 
-================================
 console.log(person.address?.city);
 console.log(person.fullName?.());
-=========================================
-*/

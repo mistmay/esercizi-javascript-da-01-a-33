3bfaac1488eaa797418d01924f624e25f25673af
Giuseppe Marchesiello <94814616+mistmay@users.noreply.github.com>
2022-02-08 19:02:24 +0100

    Update exercise-28.js

--- 28/exercise-28.js
+++ 28/exercise-28.js
@@ -1,11 +1,5 @@
 const order = {};
-/*
-I have to put the solution in a comment
-because Beautify is not recognizing the syntax
-for the optional-chain and it does not let me save
-without changing the formatation of the code
-=======================================
+
 if (!order.customer?.address?.city) {
   console.log('City is required');
 }
-============================================*/
